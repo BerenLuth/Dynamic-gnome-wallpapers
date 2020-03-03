f2338232e6b3e14ca304df8f7290b6083e226de5
Fabio <r.fabio95@gmail.com>
2020-03-03 15:19:12 +0100

    new install script that doesn't rely on svn

--- install.sh
+++ install.sh
@@ -1,16 +1,49 @@
 #! /bin/bash
 
-echo "Adding $1 to wallpapers ..."
-echo "Starting download ..."
+if [ $# -eq 0 ]
+  then
+    echo "Adding all wallpapers ..."
+else
+    echo "Adding $1 to wallpapers ..."
+fi
 
-svn checkout https://github.com/b1izzard-34/Dynamic-gnome-wallpapers/trunk/$1
+cd /tmp
+if [ -d "Dynamic-gnome-wallpapers" ]; then
+    echo "Repository already cloned, ready to install wallpapers"
+    echo ""
+else
+    echo "Starting to download ..."
+    git clone https://github.com/BerenLuth/Dynamic-gnome-wallpapers.git
+    echo "Ready to install wallpapers"
+fi
+cd Dynamic-gnome-wallpapers
 
-cd $1
-sudo mkdir /usr/share/backgrounds/gnome/$1-timed
-sudo cp $1*.jpeg /usr/share/backgrounds/gnome/$1-timed
-sudo cp $1-timed.xml /usr/share/backgrounds/gnome
-sudo cp $1.xml /usr/share/gnome-background-properties
-echo "Added $1 dynamic wallpaper!"
+if [ $# -eq 0 ]; then
+    for dir in */; do
+        dir=${dir::-1}
+        echo "- Installing $dir ..."
+        cd $dir
+        sudo mkdir -p /usr/share/backgrounds/gnome/$dir-timed
+        sudo cp $dir*.jpeg /usr/share/backgrounds/gnome/$dir-timed
+        sudo cp $dir-timed.xml /usr/share/backgrounds/gnome
+        sudo cp $dir.xml /usr/share/gnome-background-properties
+        echo "   Added $dir dynamic wallpaper!"
+        cd ..
+    done
+else
+    echo "Installing $1 ..."
+    cd $1
+    sudo mkdir -p /usr/share/backgrounds/gnome/$1-timed
+    sudo cp $1*.jpeg /usr/share/backgrounds/gnome/$1-timed
+    sudo cp $1-timed.xml /usr/share/backgrounds/gnome
+    sudo cp $1.xml /usr/share/gnome-background-properties
+    echo "Added $1 dynamic wallpaper!"
+    cd ..
+fi
 
-echo "Cleaning up ..."
-rm -rf $1
+# Uncomment the next lines to delete the folder after installation
+# Not strictly necessary since it saves the folter in /tmp
+#
+# echo "Cleaning up ..."
+# cd ..
+# rm -rf Dynamic-gnome-wallpapers
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
-echo "Removing $1 from installed wallpapers ..."
-cd $i
-sudo rm -rf /usr/share/backgrounds/gnome/$1-timed
-sudo rm -rf /usr/share/backgrounds/gnome/$1-timed.xml  
-sudo rm -rf /usr/share/gnome-background-properties/$1.xml
-echo "Removed $1 dynamic wallpaper!"
+if [ $# -eq 0 ]
+  then
+    echo "Removing all the dynamic wallpapers ..."
+    for dir in */; do
+        dir=${dir::-1}
+        sudo rm -rf /usr/share/backgrounds/gnome/$dir-timed
+        sudo rm -rf /usr/share/backgrounds/gnome/$dir-timed.xml  
+        sudo rm -rf /usr/share/gnome-background-properties/$dir.xml
+        echo "Removed $dir dynamic wallpaper!"
+    done
+else
+    echo "Removing $1 from installed wallpapers ..."
+    sudo rm -rf /usr/share/backgrounds/gnome/$1-timed
+    sudo rm -rf /usr/share/backgrounds/gnome/$1-timed.xml  
+    sudo rm -rf /usr/share/gnome-background-properties/$1.xml
+    echo "Removed $1 dynamic wallpaper!"
+fi
